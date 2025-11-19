14da788571970b16b33e6531a0c4afae50ec71f8
1709emil <1709emil@gmail.com>
2025-11-19 20:57:53 +0100

    ui changes

--- Presentation/ViewModels/MainViewModel.cs
+++ Presentation/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
 using System.Windows.Input;
 using VisualWorkflowBuilder.Application.Ports;
 using VisualWorkflowBuilder.Core.Entities;
@@ -10,16 +12,18 @@ public class MainViewModel
 {
     private IWorkflowManager manager;
 
-    public ObservableCollection<Workflow> Workflow { get; } = new();
+    
+    public ObservableCollection<Job> Jobs { get; } = new();
     
     public ICommand AddJobToWorkspaceCommand {get; set;}
-    
+    public ICommand ShowEditWindowCommand {get; set;}
     public MainViewModel(IWorkflowManager workflowManager)
     {
         manager = workflowManager;
-        Workflow.Add(manager.GetWorkflow());  
+        
         
         AddJobToWorkspaceCommand = new RelayCommand(AddJobToWorkspace, CanAddJobToWorkspace);
+        ShowEditWindowCommand = new RelayCommand(ShowEditWindow, CanShowEditWindow);
     }
 
     private bool CanAddJobToWorkspace(object parameter)
@@ -28,8 +32,24 @@ public class MainViewModel
     }
 
     private void AddJobToWorkspace(object parameter)
-    {
+    { 
+        Jobs.Add(new Job(){Name = "New Job"});
+        Console.WriteLine($"New Job: {Jobs.Count}");
         
+    }
+
+    private bool CanShowEditWindow(object parameter)
+    {
+        return true;
+    }
+
+    private void ShowEditWindow(object parameter)
+    {
+        Console.WriteLine(parameter.ToString());
+        EditJobWindow editWindow = new EditJobWindow();
+        editWindow.Owner = parameter as MainWindow;
+        editWindow.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+        editWindow.Show();
     }
     
     
---
+++ Presentation/Views/EditJobWindow.xaml.cs
@@ -0,0 +1,13 @@
+using System.Windows;
+
+
+namespace VisualWorkflowBuilder;
+
+public partial class EditJobWindow : Window
+{
+    public EditJobWindow()
+    {
+        InitializeComponent();
+       
+    }
+}
