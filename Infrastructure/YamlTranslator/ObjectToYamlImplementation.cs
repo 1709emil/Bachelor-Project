370479afd8a1ff09ddab46bf4b79045b350d5d0c
1709emil <1709emil@gmail.com>
2025-11-27 11:30:41 +0100

    made the color shcema into a shared resource + added .vs to the git ignore

--- Infrastructure/YamlTranslator/ObjectToYamlImplementation.cs
+++ Infrastructure/YamlTranslator/ObjectToYamlImplementation.cs
@@ -10,14 +10,14 @@ public class ObjectToYamlImplementation : IObjectToYamlTranslator
 {
     public void TranslateObjectToYaml(Workflow workflow)
     {
-        string OutPutPath = "C:\\Programmering\\uni\\YamlTestingForBachelor\\.github\\workflows\\";
+        string OutPutPath = "C:\\Programmering\\YamlTestingForBachelor\\.github\\workflows\\";
         
         var serializer = new SerializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance)
             .ConfigureDefaultValuesHandling(DefaultValuesHandling.OmitNull)
             .WithQuotingNecessaryStrings()
             .Build();
         string Yaml = serializer.Serialize(workflow);
-        File.WriteAllText(Path.Combine(OutPutPath, "NIG.yaml"), Yaml);
+        File.WriteAllText(Path.Combine(OutPutPath, "TestOfVisualBuilder.yaml"), Yaml);
         Console.WriteLine(Yaml);
     }
     
